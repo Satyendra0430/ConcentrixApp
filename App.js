9706c8dd70962eae34558af746ee63d02ce1325a
Satyendra0430 <60212037+Satyendra0430@users.noreply.github.com>
2022-02-17 13:33:50 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+export default class App extends React.Component {
+  render(){
+    return(
+      <View style={{justifyContent:'center', alignItems:'center', flex:1}}>
+      <Text>{'Hii'}</Text>
+    </View>
+
+    )
+  }
+}
